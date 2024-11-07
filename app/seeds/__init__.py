c38c65313ef2dc621f5fbdcd1113db1504ddb009
Mila7158 <liudmila7158@gmail.com>
2024-11-07 15:44:27 -0800

    seeds fovorites

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .favorites import seed_favorites, undo_favorites
 
 from app.models.db import db, environment, SCHEMA
 
@@ -17,7 +18,9 @@ def seed():
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
+        undo_favorites()
     seed_users()
+    seed_favorites()
     # Add other seed functions here
 
 
@@ -25,4 +28,5 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_favorites()
     # Add other undo functions here
