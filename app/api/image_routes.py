79d5e405667ea34ad731978cc2614d08124a9d10
gavinjcampbell83 <gavinjcampbell83@gmail.com>
2024-11-07 11:20:02 -0700

    pausing aws development

--- app/api/image_routes.py
+++ app/api/image_routes.py
@@ -19,7 +19,7 @@ def upload_image():
     if form.validate_on_submit():
           
         image = form.data["image"]
-        image.filename = get_unique_filename(image.filename)
+        # image.filename = get_unique_filename(image.filename)
         upload = upload_file_to_s3(image)
         print(upload)
 
