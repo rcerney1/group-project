787bbbf6317122bef470cc7768ae819a86d899b6
zhengzhao <zhaozi486@gmail.com>
2024-11-10 15:24:34 -0800

    add style for login form

--- react-vite/src/components/LoginFormModal/LoginFormModal.jsx
+++ react-vite/src/components/LoginFormModal/LoginFormModal.jsx
@@ -32,35 +32,37 @@ function LoginFormModal() {
     setEmail('demo@aa.io')
     setPassword('password')
   }
-  
+
   return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
+      <div className="login_form_container">
+        <h1>Log In</h1>
+        <form onSubmit={handleSubmit}>
         <label>
           Email
           <input
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            className={errors.email ? "error" : ""}
             required
           />
         </label>
-        {errors.email && <p>{errors.email}</p>}
+        {errors.email && <p className="error">{errors.email}</p>}
         <label>
           Password
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            className={errors.password ? "error" : ""}
             required
           />
         </label>
-        {errors.password && <p>{errors.password}</p>}
+        {errors.password && <p className="error">{errors.password}</p>}
         <button type="submit">Log In</button>
-        <button onClick={handleDemoLogin}>Demo User</button>
+        <button onClick={handleDemoLogin}>Demo User Login</button>
       </form>
-    </>
+    </div>
   );
 }
 
