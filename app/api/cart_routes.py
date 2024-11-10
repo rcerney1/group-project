8da23f1fe6c0992d6bae9ec4f85894e524ee50f4
zhengzhao <zhaozi486@gmail.com>
2024-11-09 22:59:07 -0800

    cart routes...

--- app/api/cart_routes.py
+++ app/api/cart_routes.py
@@ -8,8 +8,8 @@ cart_routes = Blueprint('carts', __name__)
 
 def get_cart():
     '''
-    helper function to get the current user's cart
-    if the cart does not exist, create a new cart
+    Helper function to get the current user's cart
+    If the cart does not exist, create a new cart
     '''
     cart = Cart.query.filter(Cart.user_id == current_user.id).first()
     if not cart:
@@ -38,20 +38,68 @@ def view_cart():
 def add_item():
     '''
     Add a new item to cart
-    increment quantity if item already exists in cart
+    Increment quantity if item already exists in cart
     '''
+    data = request.get_json()
+    product_id = data.get('product_id')
+    quantity = data.get('quantity', 1) # default quantity is 1
+
+
+    if not product_id:
+        return {'message': 'Product ID is required'}, 400
+    
+    if quantity < 1:
+        return {'message': 'Quantity must be greater than 0'}, 400
+    
+    product = Product.query.get(product_id)
+    if not product:
+        return {'message': 'Product not found'}, 404
+
+    cart = get_cart()
+
+    cart_item = CartItem.query.filter(
+        CartItem.cart_id == cart.id,
+        CartItem.product_id == product_id
+    ).first()
+
+    if cart_item:
+        cart_item.quantity += quantity
+    else:
+        cart_item = CartItem(cart_id=cart.id, product_id=product_id, quantity=quantity)
+        db.session.add(cart_item)
+    
+    db.session.commit()
+    return {'message': 'Item added to cart', 'cart': cart.to_dict()}, 201
 
 
 
 
 @cart_routes.route('/items/<int:item_id>', methods=['PUT'])
 @login_required
-def update_item(item_id):
+def update_item_quantity(item_id):
     '''
     Update cart item quantity
     '''
+    cart = get_cart()
+    cart_item = CartItem.query.get(item_id)
+    
+    if not cart_item:
+        return {'message': 'Cart item not found'}, 404
+    
+    # check if the cart item belongs to the current user
+    if cart_item.cart_id != cart.id:
+        return {'message':'Forbidden'}, 403
+    
+    data = request.get_json()
+    new_quantity = data.get('quantity')
 
+    if new_quantity is None or new_quantity < 1:
+        return {'message': 'Invalid quantity'}, 400
+    
+    cart_item.quantity = new_quantity
+    db.session.commit()
 
+    return {'message': 'Item quantity updated successfully', 'cart': cart.to_dict()}, 200
 
 
 
@@ -68,8 +116,8 @@ def remove_item(item_id):
         return {'message': 'Cart item not found'}, 404
     
     # check if the cart item belongs to the current user
-    if not cart_item.carts.user_id == current_user.id:
-        return {'message':'forbidden'}, 403
+    if cart_item.carts.user_id != current_user.id:
+        return {'message':'Forbidden'}, 403
     
     db.session.delete(cart_item)
     db.session.commit()
